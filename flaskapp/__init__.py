cf2b67c59264f6cdd075f3953aa4301b1d33b3c7
sangeun.cho <kv7095@naver.com>
2023-09-04 14:04:06 +0900

    python에서 flask 직접 구동

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -27,7 +27,7 @@ import sys
 app.debug = True
 
 # server_name를 도메인 사용으로 지정하면 ip로는 연결되지 않는다. 
-app.config['SERVER_NAME'] = 'local.com:5001'   # hosts에 localhost로 등록한 이름 
+# app.config['SERVER_NAME'] = 'chosangeun-virtual.me:5000'   # hosts에 localhost로 등록한 이름 
 
 # jinja 사용구문에서 개행되는 것을 방지  
 app.jinja_env.trim_blocks = True 
