1f73a880ba6ff9d4b1616b6bdc3dbdd07747e89c
sangeun.cho <kv7095@naver.com>
2023-09-06 15:54:42 +0900

    Apache/mod_wsgi 환경에서 sys.stdout를 열려고 하면 OSError가 발생하여 원복

--- flaskapp/utils.py
+++ flaskapp/utils.py
@@ -1,5 +1,5 @@
 from flaskapp import app, date, datetime, os, url_for
-import sys
+# import sys
 def make_date(dt, fmt):
     print('make_date.dt:', dt)
     if not isinstance(dt, date):
@@ -13,7 +13,9 @@ def override_url_for():
     print("context_processor.override_url_for")
     return dict(url_for=dated_url_for)
 
-sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
+# Apache/mod_wsgi 환경에서 sys.stdout은 표준 출력 스트림이 아니며 일반적으로 파일 디스크립터로 열린 것이 아님. 
+# 따라서 이 방식으로 sys.stdout를 열려고 하면 OSError가 발생할 수 있다.
+# sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
